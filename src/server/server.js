24277c0d19e79ecef2dc48cbd4e36c2927715f22
dheu122 <davidheu@ymail.com>
2017-09-21 09:53:39 -0700

    add basic server implementation with socket watchers

--- src/server/server.js
+++ src/server/server.js
@@ -1,3 +1,50 @@
 // Server-related functions will go here
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socket = require('socket.io');
 
-console.log("This is a server file");
+var app = express();
+var server = http.Server(app);
+var io = socket(server);
+
+var players = [];
+var rooms = [];
+
+app.set('port', 4200);
+app.use('/', express.static(path.join(__dirname, '../../')));
+
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname, '../../index.html'));
+});
+
+server.listen(4200, function() {
+    console.log('Starting server on port 4200');
+});
+
+io.on('connection', function(socket) {
+
+    socket.on('ConnectToServer', function(playerData) {
+        var data = playerData; // information such as nicknames, location?, character
+
+        players.push({
+            name: data.name,
+            id: socket.id,
+        });
+
+        console.log('Connection id: ' + players[players.length-1].id + ' has connected to the server');
+    })
+
+    socket.on("DisconnectFromServer", function(playerData) {
+        var data = playerData;
+
+        for(var i = 0; i < players.length; i++) {
+            if(players[i].id == socket.id) {
+                players.splice(i, 1);
+            }
+        }
+
+        console.log('Connection id: ' + socket.id + 'has disconnected from the server');
+    })
+});
+
