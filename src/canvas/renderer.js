f66a07e0507e8e671334dd36a3e180090756eb45
dheu122 <davidheu@ymail.com>
2017-09-26 16:32:03 -0700

    add basic render, game loop, character

--- src/canvas/renderer.js
+++ src/canvas/renderer.js
@@ -1,7 +1,125 @@
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext("2d");
+
+ctx.imageSmoothingEnabled = false;
+ctx.scale(3,3);
+
 var Renderer = {
+    // Images from our assets folder will go here
+    Images: {
+        player: 'assets/player_test.png'
+    },
 
     // Sprite, Tilemaps, and Animation function objects will go here
+    Sprite: function(options) {
+        var context = ctx;
 
+        this.image = new Image();
+        this.image.src = options.image;
+
+        this.x = options.x;
+        this.y = options.y;
+
+        this.isSpriteSheet = options.isSpriteSheet;
+        this.width = options.width;
+        this.height = options.height;
+        this.index = options.index;
+
+        if(this.x == null) { this.x = 0; }
+        if(this.y == null) { this.y = 0; }
+
+        this.render = function() {
+            if(this.image == null) { console.log("Image parameter is missing from render()"); return;}
+            if(this.isSpriteSheet) {
+                if(this.width != null && this.height != null && this.index != null) {
+                    var imgWidth = this.image.width;
+                    var imgHeight = this.image.height;
+                    var xTiles = imgWidth / this.width;
+                    var yTiles = imgHeight / this.height;
+
+                    var curTile = 0;
+                    for(var row = 0; row < yTiles; row++) {
+                        for(var col = 0; col < xTiles; col++) {
+                            if(this.index == curTile) {
+                                context.drawImage(this.image, col * this.width, row * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
+                                return;
+                            } 
+                            else {
+                                curTile++;
+                            }
+                        }
+                    }
+                } 
+                else {
+                    console.log("Width, height, or index parameter is missing from render()");
+                }
+            } 
+            else {
+                context.drawImage(this.image, this.x, this.y);
+            }
+        }
+
+        this.animate = function(startIndex, endIndex, animateSpeed, animateType) {
+            if(!this.isSpriteSheet) {
+                console.log("You cannot animate a single sprite, set isSpriteSheet to true");
+            } 
+            else {
+                var _this = this;
+                var i = startIndex;
+                setInterval(function() {
+                    context.clearRect(_this.x, _this.y, _this.width, _this.height);
+                    if(startIndex < endIndex) {
+                        if(i < endIndex) {
+                            i++;
+                            _this.setIndex(i);
+                        } 
+                        else {
+                            if(animateType == 'pingpong') {
+                                var temp = startIndex;
+                                startIndex = endIndex;
+                                endIndex = temp;
+                                i--;
+                                _this.setIndex(i);
+                            }
+                            else if (animateType == 'loop') {
+                                i = startIndex;
+                                _this.setIndex(i);
+                            }
+                        }
+                    }
+                    else if (startIndex > endIndex) {
+                        if(i > endIndex) {
+                            i--;
+                            _this.setIndex(i);
+                        }
+                        else {
+                            if(animateType == 'pingpong') {
+                                var temp = startIndex;
+                                startIndex = endIndex;
+                                endIndex = temp;
+                                i++;
+                                _this.setIndex(i);
+                            }
+                            else if (animateType == 'loop') {
+                                i = startIndex;
+                                _this.setIndex(i);
+                            }
+                        }
+                    }
+                    _this.render();
+                }, 1000 / animateSpeed);
+            }
+        }
+
+        this.setIndex = function(i) {
+            this.index = i;
+            this.render();
+        }
+    },
+
+    Tilemap: function(options) {
+        
+    }
 }
 
 // Manual browser testing functions will go here
--- src/logic/game.js
+++ src/logic/game.js
@@ -1,11 +1,40 @@
 // This is the compiled game. Should be empty for now.
+var socket = io();
 
+// Example of connecting to the server, uses 'ConnectToServer' from server.js
+socket.emit('ConnectToServer', {name: 'Bilbo Baggins'});
+
+// Creates a new player 'character', and renders a sprite
+var player = new Logic.character({
+	sprite: new Renderer.Sprite({
+		image: Renderer.Images.player,
+		width: 16,
+		height: 16,
+		isSpriteSheet: true,
+		x: 0,
+		y: 0,
+		index: 0
+	}),
+	speed: 2,
+	stamina: 100
+});
+
+window.onload = function() {
+	gameLoop();
+}
+
+// Clears the screen
+// Calls the player's update() function and redraws itself
+// Repeat
+var lastLoopRun = 0;
 function gameLoop() { //this is the main game loop, i found a version of it in a tutorial, basically repeats every 2 miliseconds and runs at 33 fps 1000ms/30 = 33.3
-	if (new Date().getTime() - lastLoopRun > 30) {
-	//updatePositions();
-	//handleControls();
-	//showSprites();
-	lastLoopRun = new Date().getTime();
+	if (new Date().getTime() - lastLoopRun > 15) {
+		//updatePositions();
+		//handleControls();
+		//showSprites();
+		ctx.clearRect(0, 0, 480, 320);
+		player.update();
+		lastLoopRun = new Date().getTime();
 	}
 	setTimeout('gameLoop();', 2);
 }
