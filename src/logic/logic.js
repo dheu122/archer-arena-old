6521465fc01734cef0afc8c1a5f14d300bb55a8f
David <davidheu@ymail.com>
2017-09-26 22:36:40 -0700

    test merge

--- src/logic/logic.js
+++ src/logic/logic.js
@@ -27,32 +27,61 @@ var Logic = {
 
     //stamina: 100,
 
+    character: function(options) {
+        this.sprite = options.sprite;
+
+        this.speed = options.speed;
+
+        this.curStamina = 0;
+        this.maxStamina = options.stamina;
+        
+        this.canDodge = 0
+        this.arrowCount = 0;
+
+        this.update = function() {
+            this.sprite.render();
+            if(Logic.rightPressed) {
+                //this.sprite.animate(0, 3, 10, 'loop');
+                this.sprite.x += this.speed;
+            }
+            if(Logic.leftPressed) {
+                this.sprite.x -= this.speed;
+            }
+            if(Logic.upPressed) {
+                this.sprite.y -= this.speed;
+            }
+            if(Logic.downPressed) {
+                this.sprite.y += this.speed;
+            }
+        }
+    },
+
     keyDownHandler: function(e) {
         if(e.keyCode == Controls.rightKey) {
-            rightPressed = true;
+            Logic.rightPressed = true;
         }
         if(e.keyCode == Controls.leftKey) {
-            leftPressed = true;
+            Logic.leftPressed = true;
         }
         if(e.keyCode == Controls.upKey) {
-            upPressed = true;
+            Logic.upPressed = true;
         }
         if(e.keyCode == Controls.downKey) {
-            downPressed = true;
+            Logic.downPressed = true;
         }
     },
     keyUpHandler: function(e) {
         if(e.keyCode == Controls.rightKey) {
-            rightPressed = false;
+            Logic.rightPressed = false;
         }
         if(e.keyCode == Controls.leftKey) {
-            leftPressed = false;
+            Logic.leftPressed = false;
         }
         if(e.keyCode == Controls.upKey) {
-            upPressed = false;
+            Logic.upPressed = false;
         }
         if(e.keyCode == Controls.downKey) {
-            downPressed = false;
+            Logic.downPressed = false;
         }
     },
     mouseDownHandler: function(e) {
