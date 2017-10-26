2e872a0507d294f62ae8eb0c81a6618f25205855
Matthew Mello <matthewmello@swuser-129-8-216-178.swlan.csufresno.edu>
2017-10-26 16:37:58 -0700

    modified sprite sheet a little, Starting to develop animator component

---
+++ src/canvas/animator.js
@@ -0,0 +1,28 @@
+//This file will handle animations, their states, and transitions between
+//states
+
+//Animator will be a component of an object (i.e. player, tile, arrow,
+//ambient wildlife?)
+
+//Animator's job will be to handle various states of animation of an object
+
+//An animation will consist of various frames strung together and played in
+//a loop or in specified sequence.
+    //animation variables
+    //speed
+    //starting index
+    //ending index
+    //animation type
+
+//A state will be a specific instance that will correspond to a specific
+//animation (i.e. dying, running right, etc.)
+
+//A trasition will be a switch between states of animation based on specific
+//conditions (ex TransitionTo: idle Condition: IsMoving = false)
+//Conditions will be a list (array) of condition checks
+
+//Parameters of animation are checked before deciding to transition
+//between states or not.
+
+//Parameters will be bools for now..
+//(ex. IsMoving, IsShooting, DirectionOfMovement)
