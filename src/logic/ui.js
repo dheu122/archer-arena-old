08d36044fe7cd107336c899a1f262537358f7a08
sahil259 <sahil259@mail.fresnostate.edu>
2017-11-02 16:20:44 -0700

    added a profanity checker

--- src/logic/ui.js
+++ src/logic/ui.js
@@ -4,9 +4,24 @@ var UI = {
     getUsername: function() {
         var nickname = document.getElementById('username').value;
 
-        socket.emit('ConnectToServer', {name: nickname});
-        document.getElementById('titlescreen').remove();
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function() {
+          if ((request.readyState == 4) && (request.status == 200)) {// if DONE and SUCCESS
+                var profanityJson = JSON.parse(request.responseText);
+                var profanityList = profanityJson.profanity;
+                for(var i = 0; i < profanityList.length; i++) {
+                    if(nickname.includes(profanityList[i])) {
+                        alert("The word '" + profanityList[i] + "' was found in your name, please enter another one");
+                        return;
+                    }
+                }
+                socket.emit('ConnectToServer', {name: nickname});
+                document.getElementById('titlescreen').remove();
+            }
+        }
+        request.open("GET", "../../assets/profanity.json", true);
+        request.send();
     }
+
 }
-    
 // Manual browser testing functions will go here
